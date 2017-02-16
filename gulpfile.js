c0101eec8290f7f1390fc6f549d30d45c9b90306
Desarrollo Ondigital <ondigital.desarrollo@gmail.com>
2017-02-16 08:42:07 -0500

    Initial Commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp 			      = require('gulp'),
+    	sass   			    = require('gulp-sass'),
+    	pug				      = require('gulp-pug'),
+    	autoprefixer	  = require('autoprefixer'),
+      browserSync     = require('browser-sync').create();
+
+/* Server */
+gulp.task('default',['css'], function() {
+	browserSync.init({
+		server: './dist/'
+	});
+	gulp.watch('./dev/scss/**/*.scss', ['css']);
+  gulp.watch('./dist/*html').on('change', browserSync.reload);
+  gulp.watch('./dev/**/*.pug', ['pug']);
+});
+
+/* CSS */
+gulp.task('css', function() {
+    gulp.src('./scss/**/*.scss')
+      .pipe(sass({
+      	outputStyle: 'compress'
+      }))
+      .pipe(gulp.dest('./dist/css/'))
+      .pipe(browserSync.stream());
+});
+
+/* PUG */
+gulp.task('pug', function(){
+  gulp.src('./dev/*.pug')
+  .pipe(pug({
+    pretty: true
+  }))
+  .pipe(gulp.dest('./dist/'))
+});
